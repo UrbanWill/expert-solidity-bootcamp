2e74fe71cecb775bf58a695f2423ca45136e506b
WillUrban <will.urban.dev@gmail.com>
2023-10-11 16:53:52 +0700

    test: added fuzzing, test setter fn and constructor

--- lesson-2/test/Homework2Test.s.sol
+++ lesson-2/test/Homework2Test.s.sol
@@ -13,39 +13,61 @@ contract HomeWork2Test is Test {
     event ItemRemoved(uint256 indexed itemRemoved, uint256 indexed itemIndex);
 
     Homework2 homework2;
+    uint256 constant INITIAL_NUMBERS_LENGTH = 2;
 
     function setUp() external {
         DeployHomework2 deployer = new DeployHomework2();
-        (homework2) = deployer.run();
+
+        uint256[] memory luckyNumbers = new uint256[](INITIAL_NUMBERS_LENGTH);
+        luckyNumbers[0] = 33;
+        luckyNumbers[1] = 12;
+        (homework2) = deployer.run(luckyNumbers);
     }
 
-    function testRemoveIndexAtReverts() public {
+    /**
+     * @dev helper modifier to fuzz numbers array
+     * @param _numbers fuzzed uint256[] from foundry
+     */
+
+    modifier pupulateArr(uint256[] calldata _numbers) {
+        vm.assume(_numbers.length > 1);
+        homework2.setNumbers(_numbers);
+        _;
+    }
+
+    function testConstructorNumbersWorks() public view {
+        uint256 numbersLength = homework2.getNumbersLength();
+        assert(numbersLength == INITIAL_NUMBERS_LENGTH);
+    }
+
+    function testSetNumbersCorrectlySetNumbers(uint256[] calldata _numbers) public pupulateArr(_numbers) {
+        assert(homework2.getNumbersLength() == _numbers.length);
+    }
+
+    function testRemoveIndexAtReverts(uint256[] calldata _numbers) public pupulateArr(_numbers) {
         uint256 numbersLength = homework2.getNumbersLength();
         vm.expectRevert(Homework2.Homework2__IndexOutOfBounds.selector);
         homework2.removeIndexAt(numbersLength + 1);
     }
 
-    function testRemoveIndexAtRemovesItem() public {
-        uint256 indexToRemove = 3;
-        uint256 itemToBeRemoved = homework2.numbers(indexToRemove);
-
-        homework2.removeIndexAt(indexToRemove);
-
-        uint256 intemAtIndexAfterRemoval = homework2.numbers(indexToRemove);
-        assert(itemToBeRemoved != intemAtIndexAfterRemoval);
-    }
-
-    function testRemoveIndexAtArrayLengthDecreases() public {
+    function testRemoveIndexAtArrayLengthDecreases(uint256[] calldata _numbers, uint256 indexToRemove)
+        public
+        pupulateArr(_numbers)
+    {
+        vm.assume(indexToRemove < _numbers.length);
         uint256 numberLengthPrev = homework2.getNumbersLength();
 
-        homework2.removeIndexAt(7);
+        homework2.removeIndexAt(indexToRemove);
 
         uint256 numberLengthNext = homework2.getNumbersLength();
         assert(numberLengthNext == numberLengthPrev - 1);
     }
 
-    function testRemoveIndexAtEmitsEvent() public {
-        uint256 indexToRemove = 3;
+    function testRemoveIndexAtEmitsEvent(uint256[] calldata _numbers, uint256 indexToRemove)
+        public
+        pupulateArr(_numbers)
+    {
+        vm.assume(indexToRemove < _numbers.length);
         uint256 itemToBeRemoved = homework2.numbers(indexToRemove);
 
         vm.expectEmit(true, true, false, false, address(homework2));
